3be55c9594d55558c5b9967eb3c841915427dc86
Yab <yabuman46@gmail.com>
2024-02-12 10:04:19 +0300

    feat: Edges of a compound
    Has property edge relation ship between a compoun and a discriptor. This doesn't include the source and target id yet because the target, descriptor nodes, doesnt' exit yet.

--- adapters/compound_adapter.py
+++ adapters/compound_adapter.py
@@ -18,8 +18,15 @@ class CompoundAdapter(Adapter):
 
     COMPOUNDS = {}
 
-    def __init__(self, filepath=None, label="compound", dry_run=False):
-        self.label = label
+    def __init__(
+        self,
+        filepath=None,
+        node_label="compound",
+        edge_label="has_property",
+        dry_run=False,
+    ):
+        self.node_label = node_label
+        self.edge_label = edge_label
         if not filepath:
             logger.error("Input file not specified")
             raise ValueError("Input file not specified")
@@ -53,13 +60,11 @@ class CompoundAdapter(Adapter):
     def __get_graph(self, ontology):
         onto = get_ontology(CompoundAdapter.COMPOUNDS[ontology]).load()
         self.graph = default_world.as_rdflib_graph()
-        self.__clear_cache()
         return self.graph
 
     def get_nodes(self):
         for ontology in CompoundAdapter.COMPOUNDS.keys():
             self.graph = self.__get_graph(ontology)
-            self.__cache_node_properties()
             subject_objects = list(self.graph.subject_objects())
 
             nodes_dict = {}
@@ -100,7 +105,99 @@ class CompoundAdapter(Adapter):
                     props["source_url"] = CompoundAdapter.SOURCE_URL
 
                 i += 1
-                yield term_id, self.label, props
+                yield term_id, self.node_label, props
+
+    def get_edges(self):
+        for ontology in CompoundAdapter.COMPOUNDS.keys():
+            self.graph = self.__get_graph(ontology)
+            subject_objects = list(self.graph.subject_objects())
+
+            nodes_dict = {}
+
+            for entry in subject_objects:
+                (subject, object) = entry
+                nodes_dict[subject] = object
+
+            nodes = nodes_dict.keys()
+            nodes = list(nodes)[:100] if self.dry_run else nodes
+
+            i = 0  # dry run is set to true just output the first 100 nodes
+            for node in tqdm(nodes, desc="Loading properties", unit="property"):
+                if i > 100 and self.dry_run:
+                    break
+
+                # avoiding blank nodes and other arbitrary node types
+                if not isinstance(node, rdflib.term.URIRef):
+                    continue
+                # if str(node) == "http://anonymous" or "@prefix":
+                #     continue
+                term_id = CompoundAdapter.to_key(node)
+                data = self.__getValue(term_id[3:])
+                # logger.info(f"Node: {node} with term id {term_id}")
+                props = {}
+                if data is None:
+                    continue
+
+                if "PC_Compounds" in data:
+                    compound_info = data["PC_Compounds"][0]
+
+                    for property in compound_info.get("props", []):
+                        prop_name = (
+                            f"{property['urn']['name']}_{property['urn']['label']}"
+                            if "urn" in property and "name" in property["urn"]
+                            else property["urn"]["label"]
+                        )
+                        prop_name = prop_name.replace(" ", "_")
+
+                        excluded_properties = [
+                            "SubStructure_Keys_Fingerprint",
+                            "Allowed_IUPAC_Name",
+                            "CAS-like_Style_IUPAC_Name",
+                            "Markup_IUPAC_Name",
+                            "Systematic_IUPAC_Name",
+                            "Traditional_IUPAC_Name",
+                            "Canonicalized_Compound",
+                        ]
+                        if prop_name in excluded_properties:
+                            continue
+
+                        # Changed to snake case
+                        if prop_name == "MonoIsotopic_Weight":
+                            prop_name = "Mono_Isotopic_Weight"
+                        if prop_name == "Polar_Surface_Area_Topological":
+                            prop_name = "TPSA"
+
+                        value_key = next(
+                            (
+                                key
+                                for key in ["sval", "ival", "binary", "fval"]
+                                if key in property["value"]
+                            ),
+                            None,
+                        )
+                        prop_value = property["value"][value_key]
+                        props[prop_name] = prop_value
+
+                    for key, count in compound_info.get("count", []).items():
+                        if key == "heavy_atom":
+                            props["Non-hydrogen_Atom_Count"] = count
+                        if key == "atom_chiral_def":
+                            props["Defined_Atom_Stereo_Count"] = count
+                        if key == "atom_chiral_undef":
+                            props["Undefined_Atom_Stereo_Count"] = count
+                        if key == "bond_chiral_def":
+                            props["Defined_Bond_Stereo_Count"] = count
+                        if key == "bond_chiral_undef":
+                            props["Undefined_Bond_Stereo_Count"] = count
+                        if key == "covalent_unit":
+                            props["Covalent_Unit_Count"] = count
+                        if key == "isotope_atom":
+                            props["Isotope_Atom_Count"] = count
+
+                    props["Total_Formal_Charge"] = compound_info.get("charge", "")
+
+                i += 1
+                yield term_id, self.edge_label, props
 
     @classmethod
     def to_key(cls, node_uri):
@@ -128,74 +225,3 @@ class CompoundAdapter(Adapter):
         except Exception as e:
             logger.error(f"{e}")
             return ""
-
-    # it's faster to load all subject/objects beforehand
-    def __clear_cache(self):
-        self.cache = {}
-
-    def __cache_node_properties(self):
-        self.cache["attributes"] = self.__cache_predicate(CompoundAdapter.HAS_ATTRIBUTE)
-        # self.cache["term_names"] = self.cache_predicate(CompoundAdapter.LABEL)
-        # self.cache["descriptions"] = self.cache_predicate(CompoundAdapter.DESCRIPTION)
-        # self.cache["has_component"] = self.cache_predicate(
-        #     CompoundAdapter.HAS_COMPONENT
-        # )
-        # self.cache["has_same_connectivity_with"] = self.cache_predicate(
-        #     CompoundAdapter.HAS_SAME_CONNECTIVITY_WITH
-        # )
-
-    def __cache_predicate(self, predicate):
-        return list(self.graph.subject_objects(predicate=predicate))
-
-        # if "PC_Compounds" in data:
-        #     compound_info = data["PC_Compounds"][0]
-        #     excluded_properties = [
-        #         "SubStructure_Keys_Fingerprint",
-        #         "Allowed_IUPAC_Name",
-        #         "CAS-like_Style_IUPAC_Name",
-        #         "Markup_IUPAC_Name",
-        #         "Systematic_IUPAC_Name",
-        #         "Traditional_IUPAC_Name",
-        #         "Canonicalized_Compound",
-        #     ]
-        #     for property in compound_info.get("props", []):
-        #         prop_name = (
-        #             f"{property['urn']['name']}_{property['urn']['label']}"
-        #             if "urn" in property and "name" in property["urn"]
-        #             else property["urn"]["label"]
-        #         )
-        #         prop_name = prop_name.replace(" ", "_")
-        #         if prop_name in excluded_properties:
-        #             continue
-        #         if prop_name == "MonoIsotopic_Weight":
-        #             prop_name = "Mono_Isotopic_Weight"
-        #         if prop_name == "Polar_Surface_Area_Topological":
-        #             prop_name = "TPSA"
-        #         value_key = next(
-        #             (
-        #                 key
-        #                 for key in ["sval", "ival", "binary", "fval"]
-        #                 if key in property["value"]
-        #             ),
-        #             None,
-        #         )
-        #         prop_value = property["value"][value_key]
-        #         props[prop_name] = prop_value
-
-        #     for key, count in compound_info.get("count", []).items():
-        #         if key == "heavy_atom":
-        #             props["Non-hydrogen_Atom_Count"] = count
-        #         if key == "atom_chiral_def":
-        #             props["Defined_Atom_Stereo_Count"] = count
-        #         if key == "atom_chiral_undef":
-        #             props["Undefined_Atom_Stereo_Count"] = count
-        #         if key == "bond_chiral_def":
-        #             props["Defined_Bond_Stereo_Count"] = count
-        #         if key == "bond_chiral_undef":
-        #             props["Undefined_Bond_Stereo_Count"] = count
-        #         if key == "covalent_unit":
-        #             props["Covalent_Unit_Count"] = count
-        #         if key == "isotope_atom":
-        #             props["Isotope_Atom_Count"] = count
-
-        #     props["Total_Formal_Charge"] = compound_info.get("charge", "")
