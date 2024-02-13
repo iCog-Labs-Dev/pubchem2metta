baf5d4e76d228975151e05066e6c77808f5ededf
TollanBerhanu <tollanberhanu@gmail.com>
2024-02-13 11:57:46 +0300

    feat: add is_isotopologue_of adapter

--- adapters/is_isotopologue_of_adapter.py
+++ adapters/is_isotopologue_of_adapter.py
@@ -1,8 +1,8 @@
 from adapters import Adapter
 from rdflib import Graph
-import os
-import urllib.request
-import gzip
+from tqdm import tqdm
+import gzip, os
+from biocypher._logger import logger
 
 
 class IsotopologueAdapter(Adapter):
@@ -10,51 +10,51 @@ class IsotopologueAdapter(Adapter):
     ALLOWED_TYPES = ['is isotopologue of']
     ALLOWED_LABELS = ['is_isotopologue_of']
 
-    def __init__(self, url, type, label):
+    def __init__(self, filepath, type, label):
         if type not in IsotopologueAdapter.ALLOWED_TYPES:
-            raise ValueError('Invalid type. Allowed values: ' +
-                             ', '.join(IsotopologueAdapter.ALLOWED_TYPES))
+            logger.error(f"Invalid type. Allowed values: {str(IsotopologueAdapter.ALLOWED_TYPES)}")
+            raise ValueError(f"Invalid type. Allowed values: {str(IsotopologueAdapter.ALLOWED_TYPES)}")
         if label not in IsotopologueAdapter.ALLOWED_LABELS:
-            raise ValueError('Invalid label. Allowed values: ' +
-                             ', '.join(IsotopologueAdapter.ALLOWED_LABELS))
+            logger.error(f"Invalid label. Allowed values: {str(IsotopologueAdapter.ALLOWED_LABELS)}")
+            raise ValueError(f"Invalid label. Allowed values: {str(IsotopologueAdapter.ALLOWED_LABELS)}")
+        if not filepath:
+            logger.error("Input file not specified")
+            raise ValueError("Input file not specified")
+        if not os.path.exists(filepath):
+            logger.error(f"Input file {filepath} doesn't exist")
+            raise FileNotFoundError(f"Input file {filepath} doesn't exist")
 
-        self.url = url # "https://ftp.ncbi.nlm.nih.gov/pubchem/RDF/compound/general/pc_compound2isotopologue.ttl.gz"
+        self.filepath = filepath # "https://ftp.ncbi.nlm.nih.gov/pubchem/RDF/compound/general/pc_compound2isotopologue.ttl.gz"
         self.dataset = label
         self.type = type
         self.label = label
         self.source = "PubChem"
         self.source_url = "http://rdf.ncbi.nlm.nih.gov/pubchem/compound/"
-
+        self.version = "1.0"
 
     def get_edges(self):
         # Download the file
-        filename = self.url.split('/')[-1]
-        filepath = os.path.join('./', filename)
-        urllib.request.urlretrieve(self.url, filepath)
+        # filename = self.url.split('/')[-1]
+        # filepath = os.path.join('./', filename)
+        # urllib.request.urlretrieve(self.url, filepath)
+        
+        g = Graph()
 
         # Extract the file
-        with gzip.open(filepath, 'rb') as f_in:
-            isotopologue_ttl = f_in.read().decode('utf-8')
+        with gzip.open(self.filepath, 'rb') as f:
+            g.parse(f, format='turtle')
 
-            # Load the Turtle file into an RDF Graph
-            g = Graph()
-            g.parse(isotopologue_ttl, format="ttl")
+        for subj, pred, obj in tqdm(g, desc="Loading compounds", unit="is_isotopologue_of"):
+            subject_id = subj.split('/')[-1]
+            object_id = obj.split('/')[-1]
 
-            for sub, _, obj in g:
-                # Extract the local name of the subject and object
-                subject_id = sub.split('/')[-1]
-                object_id = obj.split('/')[-1]
-            
-                _id = subject_id + '_' + object_id
-                _source = subject_id
-                _target = object_id
-                _props = {
-                    'source': self.source,
-                    'source_url': self.source_url
-                }
-                print(_source, _target, self.label, _props)
-                yield(_id, _source, _target, self.label, _props)
-
-        # # Delete the extracted file
-        # os.remove(filepath)
+            _id = subject_id + '_' + object_id
+            _source = subject_id
+            _target = object_id
+            _props = {
+                'source': self.source,
+                'source_url': self.source_url,
+                'version': self.version
+            }
+            yield(_id, _source, _target, self.label, _props)
 
